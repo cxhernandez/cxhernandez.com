d251012a40eb2a8e592d9594dfa9674686e1cc9f
Carlos Hernandez <cxh@stanford.edu>
2015-05-13 17:40:21 -0400

    fix gs

--- _scripts/gscrawler.py
+++ _scripts/gscrawler.py
@@ -26,12 +26,12 @@ def get_table(soup):
     titles = [item.text
               for item in table_data.findAll('a', {'class': 'gsc_a_at'})]
     authors = [item.text
-               for i, item in enumerate(table_data.findAll('div',
-                                                           {'class': 'gs_gray'}))
+               for i, item in enumerate(
+                   table_data.findAll('div', {'class': 'gs_gray'}))
                if not (i % 2)]
     journals = [item.text.split(',')[0]
-                for i, item in enumerate(table_data.findAll('div',
-                                                            {'class': 'gs_gray'}))
+                for i, item in enumerate(
+                    table_data.findAll('div', {'class': 'gs_gray'}))
                 if i % 2]
     citations = [item.text.replace(u'\xa0', u'-')
                  for item in table_data.findAll('td', {'class': 'gsc_a_c'})]
